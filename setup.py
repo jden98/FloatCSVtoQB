a2c4a8e95b9301b81e815574fa90a296a92f4e5d
Jim den Otter <jim.denotter@computronix.com>
2025-02-17 19:52:05 -0700

    rename and update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from py2exe import freeze
+py2exe_options = dict(
+                ascii=True,  # Exclude encodings
+                excludes=['_ssl',  # Exclude _ssl
+                        'pyreadline', 'difflib', 'doctest', 'locale', 
+                        'optparse', 'calendar',
+                        "Tkconstants","Tkinter","tcl"],  # Exclude standard library
+                compressed=True,  # Compress library.zip
+                )
 
-freeze(console=['convert2iif.py'])
+freeze(console=['Float2QB.py'],
+       options={'py2exe': py2exe_options}) 
