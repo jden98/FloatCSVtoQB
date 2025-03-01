dd9e583576af0b6c24fd4aded5244689698cc6a8
Jim den Otter <jim.denotter@computronix.com>
2025-03-01 12:37:40 -0700

    handle splits and lower case dict keys

--- Float2QB.py
+++ Float2QB.py
@@ -1,8 +1,8 @@
 # Script to import CSV to QB.
 
 import csv
-import msvcrt
 import os
+import re
 import sys
 import traceback
 from datetime import datetime
@@ -18,8 +18,8 @@ def Error(message: str):
     if click.get_current_context().params['debug']:
         click.secho(traceback.format_exc(), fg='red', err=True)
 
-def GetChar():
-    return msvcrt.getch().decode("utf-8").lower()
+def keysLower(inDict: dict) -> dict:
+    return {k.lower(): keysLower(v) if isinstance(v, dict) else v for k, v in inDict.items()}
 
 def LoadListsFromQB(
     sessionManager: qb.IQBSessionManager,
@@ -53,7 +53,8 @@ def LoadListsFromQB(
 def PreCheck(
     sessionManager: qb.IQBSessionManager,
     transactions: list[dict],
-    vendorName="Merchant Name",
+    vendorName="merchant name",
+    maxSplits: int | None = None,
 ) -> bool:
     """Pre-check the CSV file for valid accounts and vendors."""
     validAccounts, validVendors = LoadListsFromQB(sessionManager)
@@ -61,7 +62,13 @@ def PreCheck(
     good = True
 
     vendors = set(t[vendorName] for t in transactions)
-    accounts = set(t["GL Code ID"] for t in transactions)
+    if maxSplits:
+        accounts = set()
+        for i in range(1, maxSplits + 1):
+            accounts.update(set(t[f"line item {i} gl code id"] for t in transactions))
+    else:
+        accounts = set(t["gl code id"] for t in transactions)
+
 
     badVendors = vendors - set(validVendors)
     badAccounts = accounts - set(validAccounts)
@@ -195,7 +202,8 @@ def WalkCheckRet(checkRet: qb.ICheckRet, statusCode: int, statusSeverity: str, s
 def ProcessTransactions(
     sessionManager: qb.IQBSessionManager,
     transactions: list[dict],
-    payeeNameField: str
+    payeeNameField: str,
+    maxSplits: int | None,
 ) -> tuple[int, qb.IMsgSetResponse]:
     """Process the transaction data."""
     requestMsgSet = sessionManager.CreateMsgSetRequest("CA", 16, 0)
@@ -203,21 +211,47 @@ def ProcessTransactions(
 
     count = 0
     for trans in transactions:
-        trnsDate = datetime.strptime(trans["Transaction DateTime"], "%Y-%m-%d %H:%M:%S.%f%z")
-
-        trnsDesc = trans["Description"].strip()
+        trnsDate = datetime.strptime(trans["transaction date"], "%Y-%m-%d %H:%M:%S.%f%z")
         trnsMerch = trans[payeeNameField]
-        trnsGlcode = trans["GL Code ID"]
+        trnsTotal = float(trans["total dollars"])
+        trnsSubtotal = float(trans["transaction subtotal dollars"])
+        trnsTax = float(trans["transaction tax dollars"])
+        trnsDesc = trans["description"]
 
-        try:
-            trnsAmount = float(trans.get("Total", 0))
-            trnsSubtotal = float(trans.get("Subtotal", 0))
-            trnsTax = float(trans.get("Tax", 0))
-        except ValueError:
-            Error("Invalid number format in transaction.")
+        lineItems = []
+        # if the file has any transactions with splits...
+        if not maxSplits:
+            # if this particular transaction has no splits
+            lineItems.append({
+                        'description': trnsDesc,
+                        'total': trnsTotal,
+                        'tax': trnsTax,
+                        'glcode': trans["gl code id"]
+                    })
+        else:
+            # if this particular transaction has splits
+            for item in range(1, maxSplits + 1):
+                # is there another split
+                if trans.get(f"line item {item} gl code id", "") > "":
+                    # Find corresponding fields for this line item
+                    splitDesc = trans.get(f"line item {item} description", "")
+                    splitTotal = float(trans.get(f"line item {item} amount", 0))
+                    splitTax = float(trans.get(f"line item {item} tax Amount", 0))
+                    splitGLCode = trans.get(f"line item {item} gl code id", "")
+
+                    lineItems.append({
+                        'description': splitDesc,
+                        'total': splitTotal,
+                        'tax': splitTax,
+                        'glcode': splitGLCode
+                    })
+
+        if not lineItems:
+            Error("Transaction has no detectable amounts or splits.")
             continue
 
-        if trnsAmount < 0:
+        # if this is a deposit
+        if trnsTotal < 0:
             # trnsType = "DEPOSIT"
             depAddRq = qb.IDepositAdd(requestMsgSet.AppendDepositAddRq())
             depAddRq.DepositToAccountRef.FullName.SetValue("Float Financial")
@@ -226,8 +260,7 @@ def ProcessTransactions(
             depLineAddRq: qb.IDepositLineAdd = depAddRq.DepositLineAddList.Append()
             depositInfo = depLineAddRq.ORDepositLineAdd.DepositInfo
             depositInfo.AccountRef.FullName.SetValue(trnsGlcode)
-            depositInfo.Amount.SetValue(-1 * trnsAmount)
-
+            depositInfo.Amount.SetValue(-1 * trnsTotal)
         else:
             # trnsType = "CHEQUE"
             chkAddRq = qb.ICheckAdd(requestMsgSet.AppendCheckAddRq())
@@ -237,10 +270,12 @@ def ProcessTransactions(
             chkAddRq.PayeeEntityRef.FullName.SetValue(trnsMerch)
             chkAddRq.Memo.SetValue(trnsDesc)
 
-            expAdd: qb.IExpenseLineAdd = chkAddRq.ExpenseLineAddList.Append()
-            expAdd.AccountRef.FullName.SetValue(trnsGlcode)
-            expAdd.Amount.SetValue(trnsSubtotal)
-            expAdd.Memo.SetValue(trnsDesc)
+            for item in lineItems:
+                expAdd: qb.IExpenseLineAdd = chkAddRq.ExpenseLineAddList.Append()
+                expAdd.AccountRef.FullName.SetValue(item['glcode'])
+                expAdd.Amount.SetValue(item['total'])
+                expAdd.Memo.SetValue(item['description'])
+
             if trnsTax != 0:
                 expAddT: qb.IExpenseLineAdd = chkAddRq.ExpenseLineAddList.Append()
                 expAddT.AccountRef.FullName.SetValue("GST Accounts Receivable")
@@ -264,16 +299,16 @@ def ProcessReimbursements(
 
     count = 0
     for trans in transactions:
-        trnsDate = datetime.strptime(trans["Expense Date"], "%d/%m/%Y")
+        trnsDate = datetime.strptime(trans["expense date"], "%d/%m/%Y")
 
-        trnsDesc = trans["Description"].strip()
+        trnsDesc = trans["description"].strip()
         trnsMerch = trans[payeeNameField]
-        trnsGlcode = trans["GL Code ID"]
+        trnsGlcode = trans["gl code id"]
 
         try:
-            trnsAmount = float(trans.get("Total", 0))
-            trnsSubtotal = float(trans.get("Subtotal", 0))
-            trnsTax = float(trans.get("Tax", 0))
+            trnsAmount = float(trans.get("total", 0))
+            trnsSubtotal = float(trans.get("subtotal", 0))
+            trnsTax = float(trans.get("tax", 0))
         except ValueError:
             Error("Invalid number format in transaction.")
             continue
@@ -302,7 +337,7 @@ def ProcessReimbursements(
     return count, respMsgSet
 
 
-def process_file(inputFileName):
+def ProcessFile(inputFileName):
     """Main processing function that handles the CSV import to QB"""
     count = 0
     inputFilePath = Path(inputFileName)
@@ -313,19 +348,23 @@ def process_file(inputFileName):
             csvReader = csv.DictReader(inputFile)
 
             # load the full set of transactions into memory
-            transactions = list(csvReader)
+            transactions = [keysLower(d) for d in list(csvReader)]
 
-            if csvReader.fieldnames and "Report Name" in csvReader.fieldnames:
+            if "report name" in transactions[0]:
                 # this must be a reimbursement file
                 reimbursement = True
-                payeeNameField = "Requester"
+                payeeNameField = "requester"
             else:
                 # this must be a standard transactions file
                 reimbursement = False
-                payeeNameField = "Merchant Name"
+                payeeNameField = "accounting vendor name"
+                firstLine = transactions[0]
+                pattern = r"^line item (\d+)"
+                splits = [int(match.group(1)) for key in firstLine if (match := re.match(pattern, key))]
+                maxSplits = max(splits) if splits else None
 
         with qb.IQBSessionManager() as sessionManager:
-            if not PreCheck(sessionManager, transactions, payeeNameField):
+            if not PreCheck(sessionManager, transactions, payeeNameField, maxSplits):
                 return
 
             if reimbursement:
@@ -334,7 +373,7 @@ def process_file(inputFileName):
                 )
             else:
                 count, respMsgSet = ProcessTransactions(
-                    sessionManager, transactions, payeeNameField
+                    sessionManager, transactions, payeeNameField, maxSplits
                 )
 
         # if the response indicates success, prompt the user to delete input file
@@ -364,7 +403,7 @@ def main(input_file, debug):
             sys.exit(1)
 
     try:
-        process_file(input_file)
+        ProcessFile(input_file)
     except Exception as e:
         Error(f"Error: {str(e)}")
         sys.exit(1)
